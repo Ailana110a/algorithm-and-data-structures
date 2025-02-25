dcd51f09fde0ba33c028c1ef7d438d9a6908c115
Ailana110a <ailankakhomushku@gmail.com>
2025-02-25 23:27:51 +0700

    lesson2

--- 110a/lesson1/obj/Debug/net8.0/lesson1.AssemblyInfo.cs
+++ 110a/lesson1/obj/Debug/net8.0/lesson1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lesson1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ac5486392860799eeac578ba117b0ed6b138d8e")]
 [assembly: System.Reflection.AssemblyProductAttribute("lesson1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lesson1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
